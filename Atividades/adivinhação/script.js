1278f510d0f72ca3a1aa5c2743efeb93882c2841
Artur Paiva <artur7paiva@hotmai.com>
2023-09-24 22:01:13 -0300

    Inicio de jogo de adivinhação

---
+++ Atividades/adivinhação/script.js
@@ -0,0 +1,8 @@
+let sorteado = Math.floor((100*Math.random()));
+
+
+function buscar_numero(x){
+    let tentativa = document.getElementById("input");
+    alert(tentativa);
+    return tentativa;
+}
